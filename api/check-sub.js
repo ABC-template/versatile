15e1af16385c7e880baef371900fbe70d2e59ebb
ABC-template <62172120+ABC-template@users.noreply.github.com>
2026-06-08 23:29:41 +0400

    Remove debug console logs from check-sub.js
    Removed console logs related to version and initialization data.

--- api/check-sub.js
+++ api/check-sub.js
@@ -1,10 +1,3 @@
-console.log('VERSION 2.0 - using initData');
-
-console.log('InitData length:', initData.length);
-console.log('Bot token exists:', !!botToken);
-const user = await validateTelegramInitData(initData, botToken);
-console.log('User validation result:', user);
-
 import { validateTelegramInitData } from './_lib/telegram_auth.js';
 
 export const config = { runtime: 'edge' };
